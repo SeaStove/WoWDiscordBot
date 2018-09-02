306ade2c07e94f6ca9cb0c2b953a8a1b97b031ac
Rob <rochovian@gmail.com>
2018-09-02 02:42:38 -0400

    setting official connection to config.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const request = require('request')
-const config = require('config.js')
+const config = require('./config.js')
 
 const client = new Discord.Client();
 
