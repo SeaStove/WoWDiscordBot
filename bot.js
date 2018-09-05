b80f548bda2dce14ef8fb8c85e05fa4eff519554
Rob <rochovian@gmail.com>
2018-09-05 00:43:58 -0400

    Adding role call code

--- bot.js
+++ bot.js
@@ -9,6 +9,34 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+let getCharInfo = function(name,realm,players){
+    return new Promise(function(resolve,reject){
+        request("https://us.api.battle.net/wow/character/" + realm + "/" + name + "?fields=items,progression,quests,talents&apikey=" + apiKey, function (error, response, body) {
+                if(body){
+                    var obj = JSON.parse(body);
+                    console.log(obj.name + " Item Level: " + obj.items.averageItemLevel);
+                    players.push(obj);
+                    resolve(players);
+                } else {
+                    reject(error);
+                }
+        });
+    })
+}
+
+var races = {
+    1: "Human", 2: "Orc", 3: "Dwarf", 4: "Night Elf", 5: "Undead", 6: "Tauren",
+    7: "Gnome", 8: "Troll", 9: "Goblin", 10: "Blood Elf", 11: "Draenei",
+    22: "Worgen", 24: "Pandaren", 25: "Ally Panda", 26: "Horde Panda",
+    27: "Nightborne", 28: "Highmountain Tauren", 29: "Void Elf", 30: "Lf. Draenei", 31: "Zand. Troll",
+    32: "Kul Tiran", 34: "Dark Iron Dwarf", 36: "Mag'har Orc"
+}
+
+var classes = {
+    1: "Warrior", 2: "Paladin", 3: "Hunter", 4: "Rogue", 5: "Priest", 6: "Death Knight",
+    7: "Shaman", 8: "Mage", 9: "Warlock", 10: "Monk", 11: "Druid", 12: "Demon Hunter"    
+};
+
 
  
 
@@ -43,9 +71,93 @@ client.on('message', async message => {
                 }
             });
         });
+    } else if ( message.content === process.env.PREFIX + ", gimme a role call"){
+        var players = [];
+        message.channel.send("BEEP BEEP, HERE COMES THE CREW\n\n");
+        //Jake
+        getCharInfo("Banquish","bleeding-hollow",players).then(function(players){
+            var spec;
+            i=0;
+            players[i].talents.forEach(function(talent){
+                if(talent.selected == true){
+                    spec = talent.spec.name
+                }
+            })
+            var thumbnail = "https://render-us.worldofwarcraft.com/character/" + players[i].thumbnail
+            var output = players[i].name + ", The " + races[players[i].race] + " " + spec + " " + classes[players[i].class] + "\nItem Level: " + players[i].items.averageItemLevel
+            message.channel.send(output, {file: thumbnail});
+            return getCharInfo("Cattus","bleeding-hollow",players)
+        //me
+        }).then(function(players){
+            i++;
+            var spec;
+            players[i].talents.forEach(function(talent){
+                if(talent.selected == true){
+                    spec = talent.spec.name
+                }
+            })
+            var thumbnail = "https://render-us.worldofwarcraft.com/character/" + players[i].thumbnail
+            var output = players[i].name + ", The " + races[players[i].race] + " " + spec + " " + classes[players[i].class] + "\nItem Level: " + players[i].items.averageItemLevel
+            message.channel.send(output, {file: thumbnail});
+            return getCharInfo("Mildspeaker","zuljin",players)
+        //nate
+        }).then(function(players){
+            i++;
+            var spec;
+            players[i].talents.forEach(function(talent){
+                if(talent.selected == true){
+                    spec = talent.spec.name
+                }
+            })
+            var thumbnail = "https://render-us.worldofwarcraft.com/character/" + players[i].thumbnail
+            var output = players[i].name + ", The " + races[players[i].race] + " " + spec + " " + classes[players[i].class] + "\nItem Level: " + players[i].items.averageItemLevel
+            message.channel.send(output, {file: thumbnail});
+            return getCharInfo("Chuckjohnson","Malganis",players)
+        //Chris
+        }).then(function(players){
+            i++;
+            var spec;
+            players[i].talents.forEach(function(talent){
+                if(talent.selected == true){
+                    spec = talent.spec.name
+                }
+            })
+            var thumbnail = "https://render-us.worldofwarcraft.com/character/" + players[i].thumbnail
+            var output = players[i].name + ", The " + races[players[i].race] + " " + spec + " " + classes[players[i].class] + "\nItem Level: " + players[i].items.averageItemLevel
+            message.channel.send(output, {file: thumbnail});
+            return getCharInfo("Thrasos","bleeding-hollow",players)
+        //Dallas
+        }).then(function(players){
+            i++;
+            var spec;
+            players[i].talents.forEach(function(talent){
+                if(talent.selected == true){
+                    spec = talent.spec.name
+                }
+            })
+            var thumbnail = "https://render-us.worldofwarcraft.com/character/" + players[i].thumbnail
+            var output = players[i].name + ", The " + races[players[i].race] + " " + spec + " " + classes[players[i].class] + "\nItem Level: " + players[i].items.averageItemLevel
+            message.channel.send(output, {file: thumbnail});
+            return getCharInfo("Morgayne","bleeding-hollow",players)
+        //Jeff
+        }).then(function(players){
+            i++;        
+            var spec;
+            players[i].talents.forEach(function(talent){
+                if(talent.selected == true){
+                    spec = talent.spec.name
+                }
+            })
+            var thumbnail = "https://render-us.worldofwarcraft.com/character/" + players[i].thumbnail
+            var output = players[i].name + ", The " + races[players[i].race] + " " + spec + " " + classes[players[i].class] + "\nCharacter Level: " + players[i].level
+            message.channel.send(output, {file: thumbnail});
+            return getCharInfo("Cattus","bleeding-hollow",players)
+        })
+
     }
 
 });
+
 
  
 
